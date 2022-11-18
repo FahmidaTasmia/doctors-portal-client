8c7a24a252135a3499fa1fe5588046f2c2cb3f5c
FahmidaTasmia <tasmia1070@gmail.com>
2022-11-18 18:58:35 +0600

    appointment added

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,28 +1,38 @@
-import { format } from 'date-fns/esm';
+import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOption from './AppointmentOption';
 
-const AvailableAppointments = ({selectedDate}) => {
-    const [appointmentOptions, setAppointmentOptions]=useState([]);
+const AvailableAppointments = ({ selectedDate }) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('appointmentOptions.json')
             .then(res => res.json())
             .then(data => setAppointmentOptions(data))
-    },[])
-    return (
-        <section className='my-24 text-center text-primary font-bold'>
-           <p>you have selected date:{format(selectedDate, 'PP')} </p>
+    }, [])
 
-           <div>
-           {
+    return (
+        <section className='my-16'>
+            <p className='text-center text-secondary font-bold'>Available Appointments on {format(selectedDate, 'PP')}</p>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
                     appointmentOptions.map(option => <AppointmentOption
                         key={option._id}
                         appointmentOption={option}
-                       
+                        setTreatment={setTreatment}
                     ></AppointmentOption>)
                 }
-           </div>
+            </div>
+            {
+                treatment &&
+                <BookingModal
+                    selectedDate={selectedDate}
+                    treatment={treatment}
+                    setTreatment={setTreatment}
+                ></BookingModal>
+            }
         </section>
     );
 };
