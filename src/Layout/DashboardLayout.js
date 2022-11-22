ec4ba2d63947b61dc8bf26145d43d7ad0d548bbe
FahmidaTasmia <tasmia1070@gmail.com>
2022-11-23 01:04:31 +0600

    manage doctors  and confirmation modal and etc added

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,28 +1,32 @@
 import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../context/AuthProvider';
 
-
-
+import useAdmin from '../hooks/useAdmin';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
-
 const DashboardLayout = () => {
-
-   
+    const { user } = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email)
     return (
         <div>
-       <Navbar></Navbar>
+            <Navbar></Navbar>
             <div className="drawer drawer-mobile">
-                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle bg-orange-300" />
                 <div className="drawer-content">
                     <Outlet></Outlet>
                 </div>
                 <div className="drawer-side">
-                    <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                    <label htmlFor="dashboard-drawer" className="drawer-overlay "></label>
+                    <ul className="menu p-4 w-80 text-base-content">
                         <li><Link to="/dashboard">My Appointments</Link></li>
-                        <li><Link to="/dashboard/allusers">All users</Link></li>
-                       
+                        {
+                            isAdmin && <>
+                                <li><Link to="/dashboard/allusers">All users</Link></li>
+                                <li><Link to="/dashboard/adddoctor">Add A Doctor</Link></li>
+                                <li><Link to="/dashboard/managedoctors">Manage Doctors</Link></li>
+                            </>
+                        }
 
                     </ul>
 
