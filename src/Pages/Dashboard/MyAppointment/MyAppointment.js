ecaf3f363b79aef45b350b8fa5cb1272d028a8ab
FahmidaTasmia <tasmia1070@gmail.com>
2022-11-22 12:35:30 +0600

    dashboard admin rule created

--- src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -37,7 +37,7 @@ const MyAppointment = () => {
                     </thead>
                     <tbody>
                         {
-                            bookings.map((booking, i) => <tr key={booking._id}>
+                          bookings?.length>0 &&  bookings?.map((booking, i) => <tr key={booking._id}>
                                 <th>{i+1}</th>
                                 <td>{booking.patient}</td>
                                 <td>{booking.treatment}</td>
